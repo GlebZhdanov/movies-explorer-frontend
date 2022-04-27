45d510a5cfa246ff3953272648bb5c1ee36f3393
Gleb Zhdanov <zhdanov-gleb@mail.ru>
2022-04-27 22:11:50 +0300

    fix: Ревью

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -112,8 +112,8 @@ export class Api {
 }
 
 const api = new Api({
-  url: 'http://localhost:3001',
-  // url: "https://api.films.gleb.nomoredomains.work",
+  // url: 'http://localhost:3001',
+  url: "https://api.films.gleb.nomoredomains.work",
   headers: {
     "Content-Type": "application/json"
   }
